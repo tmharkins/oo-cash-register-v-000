aa31fac533698c71db91910dc982ecdeb2c50f7e
tmharkins <tmharkins@gmail.com>
2016-05-06 13:18:41 +0000

    nitrous-backup at Fri May  6 13:18:41 UTC 2016

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,28 @@
+class CashRegister
 
+	attr_accessor :discount, :total, :last_trxn, :items
+
+	def initialize(discount = 0)
+		@total = 0
+		@discount = discount
+		@items = []
+	end
+
+	def add_item(item, price, quantity=1)
+		@last_trxn = price
+		quantity.times do
+			@items << item
+		end
+		@total = @total + (price * quantity)
+	end
+
+	def apply_discount
+		return "There is no discount to apply." if @discount == 0
+		@total = @total - (@total * (@discount.to_f/100))
+		"After the discount, the total comes to $#{@total.to_i}."
+	end
+
+	def void_last_transaction
+		@total = @total - @last_trxn
+	end
+end
